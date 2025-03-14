80bcb0ad1b7521cac0cbdba36e2dcf7953685ffb
EPAS05 <pavelanisimov9911@gmail.com>
2025-03-15 00:37:04 +0300

    Add files via upload

--- PythonProject1/piv/authuser/views.py
+++ PythonProject1/piv/authuser/views.py
@@ -1,4 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm, LoginForm
+from .models import User
+
 
 def index_page(request):
-    return render(request, "index.html")
+    reg_form = RegistrationForm()
+    login_form = LoginForm()
+
+    if 'register' in request.POST:
+        reg_form = RegistrationForm(request.POST)
+        if reg_form.is_valid():
+            user = User(
+                full_name=reg_form.cleaned_data['full_name'],
+                email=reg_form.cleaned_data['email']
+            )
+            user.set_password(reg_form.cleaned_data['password'])
+            user.save()
+            request.session['user_id'] = user.id
+            return redirect('profile')
+
+    elif 'login' in request.POST:
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            try:
+                user = User.objects.get(email=login_form.cleaned_data['email'])
+                if user.check_password(login_form.cleaned_data['password']):
+                    request.session['user_id'] = user.id
+                    return redirect('profile')
+                login_form.add_error('password', 'Неверный пароль')
+            except User.DoesNotExist:
+                login_form.add_error('email', 'Пользователь не найден')
+
+    return render(request, 'index.html', {
+        'reg_form': reg_form,
+        'login_form': login_form
+    })
+
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def logout(request):
+    if 'user_id' in request.session:
+        del request.session['user_id']
+    return redirect('index_page')
---
+++ PythonProject1/piv/static/js/user_script.js
@@ -0,0 +1,31 @@
+﻿// Сохранение и применение темы
+function applySavedTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.body.setAttribute('data-theme', savedTheme);
+    }
+}
+
+// Инициализация при загрузке
+document.addEventListener('DOMContentLoaded', () => {
+    applySavedTheme();
+
+    document.querySelectorAll('.animate').forEach(el => {
+        el.style.opacity = 0;
+        setTimeout(() => el.style.opacity = 1, 100);
+    });
+
+    document.querySelector('.user-photo').addEventListener('click', () => {
+        alert('НЕ ТЫКАТЬ!!!');
+    });
+    document.querySelector('.star-container').addEventListener('click', () => {
+        alert('МБ ФУНКЦИЮ С ПЕРЕКИДЫВАНИЕМ ДОМОЙ');
+    });
+});
+
+// Переключение темы
+function toggleTheme() {
+    const newTheme = document.body.getAttribute('data-theme') === 'dark' ? '' : 'dark';
+    document.body.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+}
